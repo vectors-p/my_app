198601ac5a0b8071e67538c3db0f0db29b659946
Vignesh <veegnesh36@gmail.com>
2026-02-20 17:36:29 +0530

    pill button stateful

--- lib/shared/widgets/pill_button.dart
+++ lib/shared/widgets/pill_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class PillButton extends StatelessWidget {
+class PillButton extends StatefulWidget {
   final String label;
   final VoidCallback onTap;
   final Color color;
@@ -15,21 +15,39 @@ class PillButton extends StatelessWidget {
   });
 
   @override
+  State<PillButton> createState() => _PillButtonState();
+}
+
+class _PillButtonState extends State<PillButton> {
+  bool _pressed = false;
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 14),
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(50),
-        ),
-        child: Text(
-          label,
-          style: TextStyle(
-            color: textColor,
-            fontWeight: FontWeight.w600,
-            fontSize: 14,
+      onTap: widget.onTap,
+      onTapDown: (_) => setState(() => _pressed = true),
+      onTapUp: (_) => setState(() => _pressed = false),
+      onTapCancel: () => setState(() => _pressed = false),
+      child: AnimatedScale(
+        scale: _pressed ? 0.93 : 1.0,
+        duration: const Duration(milliseconds: 100),
+        curve: Curves.easeOut,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 100),
+          padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 14),
+          decoration: BoxDecoration(
+            color: _pressed
+                ? widget.color.withValues(alpha: widget.color.a * 0.8)
+                : widget.color,
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: Text(
+            widget.label,
+            style: TextStyle(
+              color: widget.textColor,
+              fontWeight: FontWeight.w600,
+              fontSize: 14,
+            ),
           ),
         ),
       ),
